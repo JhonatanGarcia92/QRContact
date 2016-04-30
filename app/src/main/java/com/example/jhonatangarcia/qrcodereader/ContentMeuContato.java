0c6921dbf7403d8b50e9ef3938dc952a3d7fc37c
Jhonatan Garcia <jhona_jhg@hotmail.com>
2016-04-30 16:51:39 -0300

    up

---
+++ app/src/main/java/com/example/jhonatangarcia/qrcodereader/ContentMeuContato.java
@@ -0,0 +1,15 @@
+package com.example.jhonatangarcia.qrcodereader;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Jhonatan Garcoa on 30/04/2016.
+ */
+public class ContentMeuContato extends View{
+
+    public ContentMeuContato(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
